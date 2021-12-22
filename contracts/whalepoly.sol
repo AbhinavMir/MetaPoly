b222d20c3bed0687863f85bb4457ecc1e721f585
Abhinav Srivastava <atg271@gmail.com>
2021-12-22 23:41:29 +0530

    WPLY token

--- contracts/whalepoly.sol
+++ contracts/whalepoly.sol
@@ -1,10 +1,40 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract WPLYtoken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("WhalePoly", "WPLY", 18) public {
-        _mint(msg.sender, initialSupply);
+contract WPLYtoken is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        address initialAccount,
+        uint256 initialBalance
+    ) payable ERC20(name, symbol) {
+        _mint(initialAccount, initialBalance);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+
+    function transferInternal(
+        address from,
+        address to,
+        uint256 value
+    ) public {
+        _transfer(from, to, value);
+    }
+
+    function approveInternal(
+        address owner,
+        address spender,
+        uint256 value
+    ) public {
+        _approve(owner, spender, value);
     }
 }
