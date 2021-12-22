462769d09a5fa712764db449b103c827f824f144
Abhinav Srivastava <atg271@gmail.com>
2021-12-22 17:00:39 +0530

    Banker scaffold

---
+++ contracts/banker.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Banker {
+    address public banker;
+    address[] public players;
+
+    constructor(address _banker, address[] _players) {
+        _banker = banker;
+        _players = players;
+    }
+
+    function addPlayers() public
+    {
+
+    }
+
+    function distributeCapital() public 
+    {
+
+    }
+}
