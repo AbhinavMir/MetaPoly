ae2d2bfd65a49689e6cd548e268e8e8d524f4245
Abhinav Srivastava <atg271@gmail.com>
2021-12-25 15:04:42 +0530

    Improved 721

--- contracts/whalepoly.sol
+++ contracts/whalepoly.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WPLYtoken is ERC20 {
     constructor(
